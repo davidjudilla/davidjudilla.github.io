5b9de8906cb62a4233fab9ea8d5bcab374c46716
David Judilla <davidfjudilla@gmail.com>
2017-04-23 19:18:12 -0700

    added projects, social medias, and tracking

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const openBtn = document.querySelector('.howIGotStarted  .openBtn')
+const gotStartedHeader = document.querySelector('.gotStartedHeader')
+const openBtn = document.querySelector('.howIGotStarted  .openBtn');
 const storySection = document.querySelector('.howIGotStarted div');
 var storyOpened = false;
-openBtn.addEventListener('click', function(e) {
+gotStartedHeader.addEventListener('click', function(e) {
 	storySection.hidden = !storySection.hidden
-	if(storySection.hidden) {
-		openBtn.classList.toggle("fa-caret-square-o-right");
-		openBtn.classList.toggle("fa-caret-square-o-down");
-	}
+	openBtn.classList.toggle("fa-caret-square-o-right");
+	openBtn.classList.toggle("fa-caret-square-o-down");
 });
